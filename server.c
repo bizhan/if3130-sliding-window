0c3d1b49ffa85eb8e8afb9e5df2182671937d2f9
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-10-18 21:45:26 +0700

    Add timeout

--- server.c
+++ server.c
@@ -37,9 +37,19 @@ void init_socket(int* udpSocket, int port){
 }
 
 void initBufferArray(BufferArray* a, int max_segment) {
-    free(a->segments);
+    // free(a->segments);
     a->segments = (segment*) malloc(max_segment * sizeof(segment));
     a->length = 0;
+}
+
+void writeToFile(char* filename, char* message, int n) {
+    for(int i=0; i<n; i++){
+        printf("WRITE : %c\n", message[i]);
+    }
+    FILE *fp;
+    fp=fopen(filename, "a+");
+    fwrite(message, sizeof(message[0]), n, fp);
+    fclose(fp);
 }
 
 void drainBufferArray(BufferArray* a, char* filename, int max_segment) {
@@ -51,6 +61,7 @@ void drainBufferArray(BufferArray* a, char* filename, int max_segment) {
     }
     // printf("\n");
     writeToFile(filename, temp, a->length);
+    free(a->segments);
     initBufferArray(a,max_segment);
 }
 
@@ -66,16 +77,6 @@ void insertBufferArray(BufferArray *a, segment aSegment, int buffersize) {
         *(a->segments + curr) = aSegment;
         a->length = a->length + 1;
     }
-}
-
-void writeToFile(char* filename, char* message, int n) {
-    for(int i=0; i<n; i++){
-        printf("WRITE : %c\n", message[i]);
-    }
-    FILE *fp;
-    fp=fopen(filename, "a+");
-    fwrite(message, sizeof(message[0]), n, fp);
-    fclose(fp);
 }
 
 void initFile(char* filename){
@@ -120,83 +121,98 @@ int main(int argc, char *argv[]){
         struct sockaddr_in client_addr;
         int client_size = sizeof(client_addr);
 
-        // receive from client
-        segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
-        len = recvfrom(udpSocket,segment_buff,SEGMENTSIZE,0,(struct sockaddr*) &client_addr, &client_size);
-        segment seg;
-        to_segment(&seg,segment_buff);
-        free(segment_buff);
+        // timeout setting
+        fd_set select_fds;
+        struct timeval timeout;
 
-        printf("[%d] segment %d caught | have a data %c\n", (int) time(0), seg.seqNum, seg.data);
-        fflush(stdout);
-        
-        if(seg.data == 0){
-            drainBufferArray(&recv_buffer,FILE_NAME, max_segment);
-            printf("FINISH ALL MSG\n");
-            break;
-        }
-        // write on file
-        // char* c_in = (char *) malloc(sizeof(char));
-        // c_in[0] = seg.data;
-        // writeToFile(FILE_NAME,c_in,1);
-        // free(c_in);
+        FD_ZERO(&select_fds);
+        FD_SET(udpSocket, &select_fds);
 
-        // insert to buffer & accept segment
-        insertBufferArray(&recv_buffer,seg,BUFLEN);
-        int next_seg;
-        int pos = LFR+1;
-        printf("%d\n", seg.seqNum);
-        for(int i=0; i<max_segment; i++){
-            printf("%d", has_ack[i]);
-        }
-        printf("\n");
-        has_ack[seg.seqNum] = 1;
-        int all_full = 1;
-        while(pos <= LAF && pos < max_segment) {
-            if (!has_ack[pos]) {
-                next_seg = pos;
-                LFR=pos-1;
-                LAF=(LFR+RWS < max_segment)?LFR+RWS:max_segment-1;
-                all_full = 0;
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+
+        if ( select(32, &select_fds, NULL, NULL, &timeout) == 0 ){  
+            printf("Select has timed out...\n");
+        } else{
+            // receive from client
+            segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
+            len = recvfrom(udpSocket,segment_buff,SEGMENTSIZE,0,(struct sockaddr*) &client_addr, &client_size);
+            segment seg;
+            to_segment(&seg,segment_buff);
+            free(segment_buff);
+
+            printf("[%d] segment %d caught | have a data %c\n", (int) time(0), seg.seqNum, seg.data);
+            fflush(stdout);
+            
+            if(seg.data == 0){
+                drainBufferArray(&recv_buffer,FILE_NAME, max_segment);
+                printf("FINISH ALL MSG\n");
                 break;
             }
-            pos++;
+            // write on file
+            // char* c_in = (char *) malloc(sizeof(char));
+            // c_in[0] = seg.data;
+            // writeToFile(FILE_NAME,c_in,1);
+            // free(c_in);
+
+            // insert to buffer & accept segment
+            insertBufferArray(&recv_buffer,seg,BUFLEN);
+            int next_seg;
+            int pos = LFR+1;
+            printf("%d\n", seg.seqNum);
+            for(int i=0; i<max_segment; i++){
+                printf("%d", has_ack[i]);
+            }
+            printf("\n");
+            has_ack[seg.seqNum] = 1;
+            int all_full = 1;
+            while(pos <= LAF && pos < max_segment) {
+                if (!has_ack[pos]) {
+                    next_seg = pos;
+                    LFR=pos-1;
+                    LAF=(LFR+RWS < max_segment)?LFR+RWS:max_segment-1;
+                    all_full = 0;
+                    break;
+                }
+                pos++;
+            }
+
+            // if next segment hit maximum allowed buffer
+            if(all_full){
+                drainBufferArray(&recv_buffer,FILE_NAME, max_segment);
+                for(int i=0; i<max_segment; i++) has_ack[i]=0;
+                LFR = -1;
+                pos = 0;
+                LAF = (LFR+RWS < max_segment)?LFR+RWS:max_segment-1;
+                next_seg = LFR+1;
+            }
+
+            printf("---------\n");
+            printf(" LFR : %d \n", LFR);
+            printf(" RWS : %d \n", RWS);
+            printf(" LAF : %d \n", LAF);
+            printf(" MAX : %d\n", max_segment);
+            printf(" --------\n");
+            printf(" Next segment number %d \n", next_seg);
+            printf("---------\n\n");
+
+            packet_ack send_ack;
+            send_ack.ack = 0x6;
+            send_ack.nextSeqNum = next_seg;
+            send_ack.checksum = 0x0;
+
+            char* raw = (char*) malloc(7*sizeof(char));
+
+            ack_to_raw(send_ack,raw);
+            send_ack.checksum = checksum_str(raw,6);
+            raw[6] = send_ack.checksum;
+
+            sendto(udpSocket,raw,7,0,(struct sockaddr*) &client_addr, client_size);
+            printf("BERHASIL SEND\n");
+            free(raw);
         }
-
-        // if next segment hit maximum allowed buffer
-        if(all_full){
-            drainBufferArray(&recv_buffer,FILE_NAME, max_segment);
-            for(int i=0; i<max_segment; i++) has_ack[i]=0;
-            LFR = -1;
-            pos = 0;
-            LAF = (LFR+RWS < max_segment)?LFR+RWS:max_segment-1;
-            next_seg = LFR+1;
-        }
-
-        printf("---------\n");
-        printf(" LFR : %d \n", LFR);
-        printf(" RWS : %d \n", RWS);
-        printf(" LAF : %d \n", LAF);
-        printf(" MAX : %d\n", max_segment);
-        printf(" --------\n");
-        printf(" Next segment number %d \n", next_seg);
-        printf("---------\n\n");
-
-        packet_ack send_ack;
-        send_ack.ack = 0x6;
-        send_ack.nextSeqNum = next_seg;
-        send_ack.checksum = 0x0;
-
-        char* raw = (char*) malloc(7*sizeof(char));
-
-        ack_to_raw(send_ack,raw);
-        send_ack.checksum = checksum_str(raw,6);
-        raw[6] = send_ack.checksum;
-
-        sendto(udpSocket,raw,7,0,(struct sockaddr*) &client_addr, client_size);
-        printf("BERHASIL SEND\n");
-        free(raw);
     }
+    free(recv_buffer.segments);
     close(udpSocket);
 
     return 0;
