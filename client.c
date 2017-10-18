38af525847b8634178b44b331f1ac6aaf42420c1
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-10-18 21:50:31 +0700

    Temporary remove timeout

--- client.c
+++ client.c
@@ -82,99 +82,85 @@ int main(int argc, char *argv[]){
     int n;
     // srand(time(NULL));
     while(1){
-        // timeout setting
-        fd_set select_fds;
-        struct timeval timeout;
-
-        FD_ZERO(&select_fds);
-        FD_SET(clientSocket, &select_fds);
-
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;
-
-        if ( select(32, &select_fds, NULL, NULL, &timeout) == 0 ){  
-            printf("Select has timed out...\n");
+        // int r = rand() % 1000;
+        int c;
+        if(!block){
+            pos=0;
+            n=0;
+            free(send_buffer.segments);
+            initBufferArray(&send_buffer,max_segment);
+            printf("[%d] prepare %d segment to buffer\n", time(0), max_segment);
+            fflush(stdout);
+            while(n < max_segment && (c = fgetc(fp)) != EOF){
+                segment seg = create_segment(n,c);
+                char* raw = (char*) malloc(SEGMENTSIZE*sizeof(char));
+                segment_to_raw(seg, raw);
+                seg.checksum = checksum_str(raw, 8);
+                send_buffer.segments[n] = seg;
+                // raw[8] = seg.checksum;
+                // for(int i=0; i<SEGMENTSIZE; i++){
+                //     buf[n*SEGMENTSIZE+i] = raw[i];
+                // }
+                n++;
+            }
+            max_segment = n;
+            printf("---------\n"); fflush(stdout);
+            printf(" buffer has %d maximum segment\n", max_segment); fflush(stdout);
+            printf(" buffer data:\n"); fflush(stdout);
+            for(int i=0; i<max_segment; i++) {
+                printf(" %c", send_buffer.segments[i].data); fflush(stdout);
+            }
+            printf("\n---------\n"); fflush(stdout);
+            block = 1;
         } else {
-            // int r = rand() % 1000;
-            int c;
-            if(!block){
-                pos=0;
-                n=0;
-                free(send_buffer.segments);
-                initBufferArray(&send_buffer,max_segment);
-                printf("[%d] prepare %d segment to buffer\n", time(0), max_segment);
-                fflush(stdout);
-                while(n < max_segment && (c = fgetc(fp)) != EOF){
-                    segment seg = create_segment(n,c);
-                    char* raw = (char*) malloc(SEGMENTSIZE*sizeof(char));
-                    segment_to_raw(seg, raw);
-                    seg.checksum = checksum_str(raw, 8);
-                    send_buffer.segments[n] = seg;
-                    // raw[8] = seg.checksum;
-                    // for(int i=0; i<SEGMENTSIZE; i++){
-                    //     buf[n*SEGMENTSIZE+i] = raw[i];
-                    // }
-                    n++;
-                }
-                max_segment = n;
-                printf("---------\n"); fflush(stdout);
-                printf(" buffer has %d maximum segment\n", max_segment); fflush(stdout);
-                printf(" buffer data:\n"); fflush(stdout);
-                for(int i=0; i<max_segment; i++) {
-                    printf(" %c", send_buffer.segments[i].data); fflush(stdout);
-                }
-                printf("\n---------\n"); fflush(stdout);
-                block = 1;
-            } else {
-                // prepare buffer segment to send
-                segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
-                segment_to_raw(send_buffer.segments[pos],segment_buff);
-                // for(int j=0; j<SEGMENTSIZE; j++){
-                //     segment_buff[j] = buf[pos*SEGMENTSIZE+j];
-                // }
-                // if(r<100){
+            // prepare buffer segment to send
+            segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
+            segment_to_raw(send_buffer.segments[pos],segment_buff);
+            // for(int j=0; j<SEGMENTSIZE; j++){
+            //     segment_buff[j] = buf[pos*SEGMENTSIZE+j];
+            // }
+            // if(r<100){
+                sendto(clientSocket,segment_buff,SEGMENTSIZE,0,(struct sockaddr *)&client_addr,sizeof(client_addr));
+            // }
+            printf("[%d] segment %d with data %c was send\n", (int) time(0), segment_buff[1], segment_buff[6]); fflush(stdout);
+            free(segment_buff);
+
+            // preapare raw to receive ACK
+            raw = (char*) malloc(ACKSIZE*sizeof(char));
+            len = recvfrom(clientSocket,raw,ACKSIZE,0,NULL, NULL);
+
+            packet_ack ack;
+            to_ack(&ack, raw);
+            free(raw);
+
+            int next_seg = ack.nextSeqNum;
+            pos = next_seg;
+            LAR=next_seg-1;
+            LFS=(LAR+SWS<max_segment)?LAR+SWS:max_segment-1;
+            printf("---------\n"); fflush(stdout);
+            printf(" RECEIVE ACK : %d\n", next_seg-1); fflush(stdout);
+            printf(" LAR : %d\n", LAR); fflush(stdout);
+            printf(" SWS : %d\n", SWS); fflush(stdout);
+            printf(" LFS : %d\n", LFS); fflush(stdout);
+            printf(" MAX : %d\n", max_segment); fflush(stdout);
+            printf("---------\n\n"); fflush(stdout);
+
+            printf("POS %d MAX %d\n", pos, max_segment); fflush(stdout);
+            if(pos>=max_segment || pos==0){
+                // finish segment
+                printf("DONE\n"); fflush(stdout);
+                block=0;
+                if(c==EOF){
+                    // send last sentinel
+                    segment s = create_sentinel();
+                    segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
+                    segment_to_raw(s,segment_buff);
                     sendto(clientSocket,segment_buff,SEGMENTSIZE,0,(struct sockaddr *)&client_addr,sizeof(client_addr));
-                // }
-                printf("[%d] segment %d with data %c was send\n", (int) time(0), segment_buff[1], segment_buff[6]); fflush(stdout);
-                free(segment_buff);
-
-                // preapare raw to receive ACK
-                raw = (char*) malloc(ACKSIZE*sizeof(char));
-                len = recvfrom(clientSocket,raw,ACKSIZE,0,NULL, NULL);
-
-                packet_ack ack;
-                to_ack(&ack, raw);
-                free(raw);
-
-                int next_seg = ack.nextSeqNum;
-                pos = next_seg;
-                LAR=next_seg-1;
-                LFS=(LAR+SWS<max_segment)?LAR+SWS:max_segment-1;
-                printf("---------\n"); fflush(stdout);
-                printf(" RECEIVE ACK : %d\n", next_seg-1); fflush(stdout);
-                printf(" LAR : %d\n", LAR); fflush(stdout);
-                printf(" SWS : %d\n", SWS); fflush(stdout);
-                printf(" LFS : %d\n", LFS); fflush(stdout);
-                printf(" MAX : %d\n", max_segment); fflush(stdout);
-                printf("---------\n\n"); fflush(stdout);
-
-                printf("POS %d MAX %d\n", pos, max_segment); fflush(stdout);
-                if(pos>=max_segment || pos==0){
-                    // finish segment
-                    printf("DONE\n"); fflush(stdout);
-                    block=0;
-                    if(c==EOF){
-                        // send last sentinel
-                        segment s = create_sentinel();
-                        segment_buff = (char*) malloc(sizeof(char)*SEGMENTSIZE);
-                        segment_to_raw(s,segment_buff);
-                        sendto(clientSocket,segment_buff,SEGMENTSIZE,0,(struct sockaddr *)&client_addr,sizeof(client_addr));
-                        free(segment_buff);
-                        printf("FINISH\n"); fflush(stdout);
-                        break;
-                    }
-                    continue;
+                    free(segment_buff);
+                    printf("FINISH\n"); fflush(stdout);
+                    break;
                 }
+                continue;
             }
         }
     }
